d59518cbac482d42db01687ce2cb42dbe740de5b
Justin Gibbs <gibbs.justin@gmail.com>
2015-08-20 12:00:15 -0600

    changed php to html

--- landing-pages/motivationaal/index.php
+++ landing-pages/motivationaal/index.php
@@ -12,9 +12,94 @@
     <script src="js/vendor/modernizr.js"></script>
   </head>
  
-  <?php require ('includes/header.php');?>
-<?php require ('includes/home-hero-area.php');?>
-    
+    <body>
+<div class="row" style="padding:10px 0 10px 0">
+<div class="large-4 columns">
+<a href="../"><img src="http://www.edmondotis.co.nz/wp-content/uploads/2012/10/elo-logo-1.jpg"></a></div>
+<div class="large-4 offset-4 columns" align="right">
+NZ or Australia (64) 021 275 0226<br/>
+US or Canada (1) 760 612 7029<br/>
+<a href="https://www.facebook.com/EdmondOtisAndAssociates"><i class="fa fa-facebook-official"></i></a> <a href="https://www.linkedin.com/in/edmondotis"><i class="fa fa-linkedin-square"></i></a> <a href="mailto:edmond@edmondotis.co.nz"><i class="fa fa-envelope"></i></a> <a href="https://plus.google.com/+EdmondotisCoNz/posts"><i class="fa fa-google-plus-square"></i></a>
+
+
+
+
+</div>
+</div>
+<div class="contain-to-grid">
+    <nav class="top-bar" data-topbar role="navigation">
+<div class="row large-12 columns">
+  <ul class="title-area">
+     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+    <li class="toggle-topbar menu-icon"></li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+    </ul>
+
+    <!-- Left Nav Section -->
+    <ul class="left">
+<li><a href="../">Home</a></li>
+<li><a href="about-us.php">About Us</a></li>
+<li><a href="motivational-speaker.php">Speaking</a></li>
+<li class="has-dropdown">
+        <a href="professional-development.php">Coaching</a>
+        <ul class="dropdown">
+          <li><a href="business-professional-coaching.php">Business and Professional</a></li>
+          <li><a href="personal-life-coaching.php">Personal and Life</a></li>
+        </ul>
+      </li>
+<li><a href="recent-clients.php">Recent Clients</a></li>
+<li><a href="/blog-template.php">Blog</a></li>
+<li><a href="contact-us.php">Contact Us</a></li>
+
+    </ul>
+  </section>
+</div>
+</nav>
+</div> <section style="color:white;background:#111D29; min-height:400px;padding:25px 0 25px 0;">
+<div class="row" padding="25px 0 0 0">
+<div class="large-6 columns"><h1 style="color:white;">Motivational Speaking</h1><p class="lead">Designed to <i>engage</i>, Edmond&apos;s presentations inspire creativity, raise moral, and incite change.</p><a href="http://edmondotis.page1orbust.com/motivational-speaker.php" class="button radius secondary">Learn More</a>
+  <a href="/blog/" class="button radius">Get Motivated!</a><br/><div>
+  <img src="img/eo-profile-karate.png" width="75px" align="left" style="margin:10px;"><p><i class="fa fa-quote-left fa-2x"></i>
+Everyone hits the target they aim at – dead center – every time.</strong> Unfortunately, most of us are either aiming too low, or else we’re simply going after the wrong target. - Edmond Otis</p></div> </div>
+<div class="large-4 offset-2 columns panel" style="margin:25px 0 0 0;">
+<form class="form-horizontal" method="post" role="form" action="order.php" name="sentMessage" id="contactForm" >
+<h4 align="center">Contact Us</h4>
+   <div class="form-group">
+      <div class="small-12">
+         <input type="text" class="form-control" id="name" name="name"
+            placeholder="Full Name">
+      </div>
+   </div>
+   <div class="form-group">
+      <div class="small-12">
+         <input type="email" class="form-control" id="email" name="email"
+            placeholder="Email" required
+ 			   		   data-validation-required-message="Please enter your email">
+      </div>
+   </div>
+   <div class="form-group">
+      <div class="small-12">
+         <input type="text" class="form-control" id="phone" name="phone"
+            placeholder="Phone">
+      </div>
+   </div>
+   
+   <div class="form-group">
+      <div class="small-12">
+         <button type="submit" class="btn success expand btn-block">Get in Touch! <i class="fa fa-paper-plane"></i></button>
+     <br/>
+<small><i class="fa fa-lock"></i> We respect your privacy and promise to <i>never</i> sell your information to 3rd parties or advertisers</small>
+	</div>
+	 </div>
+   </div>
+</form>
+</div>
+</div>
+      </section>    
 <div class="row" style="min-height:25px; padding-top:15px; padding-bottom:15px;">
 </div>
 <div class="row panel" align="left" data-equalizer>
@@ -63,4 +148,74 @@ Face to face and long-distance training</p>
 
 
 </div>
-<?php require ('includes/footer.php')?>
+<footer class="footer">
+
+  <div class="row full-width">
+
+    <div class="small-12 medium-3 large-4 columns">
+<br/>
+      <i class="fa fa-binoculars"></i>
+<br/>
+      <p>Edmond’s unusual background and real-life experience give him a perspective on success, peak performance, personal strength, communication, conflict, strategy, and problem solving that is truly unique.</p>
+
+    </div>
+
+    <div class="small-12 medium-3 large-4 columns">
+<br/>
+      <i class="fa fa-university"></i>
+<br/>
+
+      <p>He applies cutting-edge research and real-world applications to create strategies that address even the most challenging professional and personal obstacles – challenges we all face. Face to face and long-distance training.</p>
+
+    </div>
+
+    <div class="small-6 medium-3 large-2 columns">
+
+      <h4>Work With Me</h4>
+
+      <ul class="footer-links">
+
+        <li><a href="#">Speaking</a></li>
+
+        <li><a href="#">Coaching</a></li>
+
+        <li><a href="#">Clients</a></li>
+
+        <li><a href="#">Blog</a></li>
+
+        <li><a href="#">Contact</a></li>
+
+      <ul>
+
+    </div>
+
+    <div class="small-6 medium-3 large-2 columns">
+
+      <h4>Follow Me</h4>
+
+      <ul class="footer-links">
+
+
+        <li><a href="#">Facebook</a></li>
+
+        <li><a href="#">Twitter</a></li>
+
+        <li><a href="#">LinkedIn</a></li>
+
+        <li><a href="#">YouTube</a></li>
+
+      <ul>
+
+    </div>
+
+  </div>
+
+</footer>
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
